d31d75354c5deadf7fde3ec9bc77a6273392f8a6
avalos010 <luizavalos40@gmail.com>
2016-08-10 18:30:05 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $('.hadouken').hide();
+                $('.hadouken').css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+ $(document).keydown(function() {
+    if (event.which === 88) {
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+    }   
+    })
+    .keyup(function() {
+    if (event.which === 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+});
+
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
